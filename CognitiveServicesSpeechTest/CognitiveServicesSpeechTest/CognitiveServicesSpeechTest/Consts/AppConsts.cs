640b47f7a85ba35610ed8896ba96e65bb17679f3
ehud.pinhas <ehud.pinhas@scrdairy.com>
2023-01-25 08:12:32 +0200

    test

--- CognitiveServicesSpeechTest/CognitiveServicesSpeechTest/CognitiveServicesSpeechTest/Consts/AppConsts.cs
+++ CognitiveServicesSpeechTest/CognitiveServicesSpeechTest/CognitiveServicesSpeechTest/Consts/AppConsts.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 namespace CognitiveServicesSpeechTest.Consts
 {
-	public class AppConsts
-	{
+    public class AppConsts
+    {
         //Azure Cognitive Services
         public const string CognitiveServicesApiKey = "372e131f3b4c484189a6a13016d457a2";
         public const string CognitiveServicesRegion = "westeurope";
+
+        public const string test = "westeurope";
+
     }
 }
-
